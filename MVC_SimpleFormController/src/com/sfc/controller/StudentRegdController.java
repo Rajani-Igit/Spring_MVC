26c27402ff25560a02d90ecb94ef8c8e9c4507b7
T05056 <T05056@APPSTEK-E01057.appsteksolutions.internal>
2018-02-13 16:34:19 +0530

    Simple form controler

---
+++ MVC_SimpleFormController/src/com/sfc/controller/StudentRegdController.java
@@ -0,0 +1,17 @@
+package com.sfc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.validation.BindException;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.SimpleFormController;
+
+public class StudentRegdController extends SimpleFormController {
+@Override
+protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response, Object command,
+		BindException errors) throws Exception {
+	// TODO Auto-generated method stub
+	return super.onSubmit(request, response, command, errors);
+}
+}
